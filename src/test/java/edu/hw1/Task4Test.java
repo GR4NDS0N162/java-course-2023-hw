463769272a4568210f66771d73c5e1db6a48b274
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-07 19:08:11 +0300

    Do the 4 task

---
+++ src/test/java/edu/hw1/Task4Test.java
@@ -0,0 +1,46 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task4Test {
+    @Test
+    @DisplayName("123456")
+    void test1() {
+        // given
+        String s = "123456";
+
+        // when
+        String fixed = Task4.fixString(s);
+
+        // then
+        assertThat(fixed).isEqualTo("214365");
+    }
+
+    @Test
+    @DisplayName("hTsii  s aimex dpus rtni.g")
+    void test2() {
+        // given
+        String s = "hTsii  s aimex dpus rtni.g";
+
+        // when
+        String fixed = Task4.fixString(s);
+
+        // then
+        assertThat(fixed).isEqualTo("This is a mixed up string.");
+    }
+
+    @Test
+    @DisplayName("badce")
+    void test3() {
+        // given
+        String s = "badce";
+
+        // when
+        String fixed = Task4.fixString(s);
+
+        // then
+        assertThat(fixed).isEqualTo("abcde");
+    }
+}
