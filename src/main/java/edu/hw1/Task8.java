698a0ed5a7e862bb68304fe544a5e6168129d662
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-07 22:29:09 +0300

    Do the 8 task

---
+++ src/main/java/edu/hw1/Task8.java
@@ -0,0 +1,39 @@
+package edu.hw1;
+
+public class Task8 {
+    private static final int[][] offsets = {
+        {2, 1},
+        {2, -1},
+        {-2, 1},
+        {-2, -1},
+        {1, 2},
+        {1, -2},
+        {-1, 2},
+        {-1, -2},
+    };
+
+    private Task8() {
+    }
+
+    public static boolean knightBoardCapture(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    for (int[] offset : offsets) {
+                        int x = j + offset[0];
+                        int y = i + offset[1];
+
+                        if (y < 0 || y >= board.length || x < 0 || x >= board[y].length) {
+                            continue;
+                        }
+
+                        if (board[y][x] == 1) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
