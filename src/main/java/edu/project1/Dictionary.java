23c76bae146aed8067da0f27d359ac801e96da09
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-09 00:39:14 +0300

    Add getIndexes method

--- src/main/java/edu/project1/Dictionary.java
+++ src/main/java/edu/project1/Dictionary.java
@@ -1,5 +1,7 @@
 package edu.project1;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,5 +24,19 @@ public class Dictionary {
 
     public static @NotNull String getRandomWord() {
         return WORDS[new Random().nextInt(WORDS.length)];
+    }
+
+    public static @NotNull HashMap<Character, HashSet<Integer>> getIndexes(@NotNull String word) {
+        var characterIndexes = new HashMap<Character, HashSet<Integer>>();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (!characterIndexes.containsKey(word.charAt(i))) {
+                characterIndexes.put(word.charAt(i), new HashSet<>());
+            }
+
+            characterIndexes.get(word.charAt(i)).add(i);
+        }
+
+        return characterIndexes;
     }
 }
