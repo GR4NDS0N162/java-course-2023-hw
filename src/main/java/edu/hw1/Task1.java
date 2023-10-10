0c991f8667ae8100912b1a2eac43edc67b1e710d
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-10 10:16:45 +0300

    Use regex

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -6,23 +6,18 @@ public class Task1 {
     private Task1() {
     }
 
-    @SuppressWarnings("MagicNumber")
-    public static int minutesToSeconds(@NotNull String duration) {
-        String[] arr = duration.split(":");
-
+    @SuppressWarnings("MagicNumber") public static int minutesToSeconds(@NotNull String duration) {
         int invalidValue = -1;
 
-        if (arr.length != 2 || arr[0].isEmpty() || arr[1].length() != 2) {
+        if (!duration.matches("[0-9]+:[0-5][0-9]")) {
             return invalidValue;
         }
+
+        String[] arr = duration.split(":");
 
         try {
             int minutes = Integer.parseInt(arr[0]);
             int seconds = Integer.parseInt(arr[1]);
-
-            if (minutes < 0 || seconds < 0 || seconds > 59) {
-                return invalidValue;
-            }
 
             return minutes * 60 + seconds;
         } catch (NumberFormatException e) {
