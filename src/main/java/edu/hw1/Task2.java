ae8be85278c27ce46032b6e3f7641b9d69ea2135
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-12 01:20:54 +0300

    Introduce constant

--- src/main/java/edu/hw1/Task2.java
+++ src/main/java/edu/hw1/Task2.java
@@ -1,17 +1,18 @@
 package edu.hw1;
 
 public class Task2 {
+    public static final int DECIMAL_BASE = 10;
+
     private Task2() {
     }
 
-    @SuppressWarnings("MagicNumber")
     public static int countDigits(int number) {
         int count = 0;
         int n = number;
 
         do {
             count++;
-            n /= 10;
+            n /= DECIMAL_BASE;
         } while (n != 0);
 
         return count;
