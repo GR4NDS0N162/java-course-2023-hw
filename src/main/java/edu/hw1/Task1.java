08bcba305f736b2e5414b9034396cdf3b46ceb9d
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-12 01:18:18 +0300

    Introduce constant

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -3,10 +3,12 @@ package edu.hw1;
 import org.jetbrains.annotations.NotNull;
 
 public class Task1 {
+    private static final int SECONDS_IN_MINUTE = 60;
+
     private Task1() {
     }
 
-    @SuppressWarnings("MagicNumber") public static int minutesToSeconds(@NotNull String duration) {
+    public static int minutesToSeconds(@NotNull String duration) {
         int invalidValue = -1;
 
         if (!duration.matches("[0-9]+:[0-5][0-9]")) {
@@ -19,7 +21,7 @@ public class Task1 {
             int minutes = Integer.parseInt(arr[0]);
             int seconds = Integer.parseInt(arr[1]);
 
-            return minutes * 60 + seconds;
+            return minutes * SECONDS_IN_MINUTE + seconds;
         } catch (NumberFormatException e) {
             return invalidValue;
         }
